c7f80b0b41ddfb35456791c1ccc64555e751115f
BrunoFelix <brunoc.felixx@gmail.com>
2020-10-27 15:56:22 -0300

    Enviando exercício número 03 do mundo 01.

---
+++ Desafio 003.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia dois números e mostra a soma entre eles, utilizando os tipos primitivos
+n1 = int(input('Digite um primeiro número: '))
+n2 = int(input('Digite um segundo número: '))
+s = n1 + n2
+print('A soma entre {} e {} será {}'.format(n1, n2, s))
+print(type(n1))
